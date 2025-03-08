502effbbd6f0a023eabd6c51d4009093738838a0
Nayla Hanegan <NaylaHanegan@gmail.com>
2025-03-08 18:04:20 -0500

    actually fix it sorrty

--- Source/Core/Core/MarioPartyNetplay/Gamestate.cpp
+++ Source/Core/Core/MarioPartyNetplay/Gamestate.cpp
@@ -5,6 +5,7 @@
 
 #include "Gamestate.h"
 #include "Core/System.h"
+#include "Core/ConfigManager.h"
 
 #include <chrono>
 static auto lastTriggerTime = std::chrono::steady_clock::now();
@@ -158,7 +159,8 @@ bool mpn_update_state()
 #define OSD_PUSH(a) mpn_push_osd_message("Adjusting #a for " + CurrentState.Scene->Name);
 void mpn_per_frame()
 {
-  if (CurrentState.IsMarioParty)
+  
+  if (SConfig::GetInstance().GetGameID() == "GMPE01" || SConfig::GetInstance().GetGameID() == "GP5E01" || SConfig::GetInstance().GetGameID() == "GP6E01" || SConfig::GetInstance().GetGameID() == "GP7E01" || SConfig::GetInstance().GetGameID() == "RM8E01" || SConfig::GetInstance().GetGameID() == "GMPEDX")
   {
     uint8_t Needs = 0;
 
