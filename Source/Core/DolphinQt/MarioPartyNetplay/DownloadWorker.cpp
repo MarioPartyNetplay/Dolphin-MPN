0e612f4f1620a392c0a219fbdc1effadd414591d
Nayla Hanegan <NaylaHanegan@gmail.com>
2025-05-22 08:33:40 -0400

    lol

--- Source/Core/DolphinQt/MarioPartyNetplay/DownloadWorker.cpp
+++ Source/Core/DolphinQt/MarioPartyNetplay/DownloadWorker.cpp
@@ -49,8 +49,7 @@ void DownloadWorker::startDownload()
     }
 }
 
-
-void DownloadWorker::updateProgress(s64 dlnow, s64 dltotal)
+void DownloadWorker::updateProgress(qint64 dlnow, qint64 dltotal) // Change s64 to qint64
 {
     emit progressUpdated(dlnow, dltotal); // Emit progress signal
     qDebug() << "Download progress:" << dlnow << "/" << dltotal; // Debug statement
