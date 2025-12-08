e85387e12cd44b9b3a2d0ccee46ec8343b49944f
Tabitha Hanegan <tabitha@tabithahanegan.com>
2025-12-07 19:51:10 -0500

    der

--- Distribution/appimage.sh
+++ Distribution/appimage.sh
@@ -1,36 +1,52 @@
-!/bin/bash
+#!/bin/bash
 
 # Environment variables
-export ARCH=x86_64
+if [ "$(uname -m)" = "x86_64" ];
+then
+    export ARCH=x86_64
+    echo "CPU architecture detected as x86_64."
+
+elif [ "$(uname -m)" = "aarch64" ];
+then
+    export ARCH=aarch64
+    echo "CPU architecture detected as aarch64."
+
+else
+    echo "CPU architecture not supported or detected."
+    exit 1
+fi
+
 export APPIMAGE_EXTRACT_AND_RUN=1
-export QMAKE=/usr/lib/qt6/bin/qmake
+export QMAKE=$(which qmake6)
 
 # Prepare the AppDir
 DESTDIR=./AppDir ninja install
 mkdir -p ./AppDir/usr/Source/Core
 cp -r ./Source/Core/DolphinQt ./AppDir/usr/Source/Core
 rm -rf ./AppDir/usr/Source/Core/DolphinQt/CMakeFiles
-rm -rf ./AppDir/usr/Source/Core/DolphinQt/dolphin-emu_autogen
+rm -rf ./AppDir/usr/Source/Core/DolphinQt/dolphin-mpn_autogen
 rm ./AppDir/usr/Source/Core/DolphinQt/cmake_install.cmake
-rm ./AppDir/usr/bin/dolphin-emu-nogui
-xxmv ./AppDir/usr/share/dolphin-emu/sys ./AppDir/usr/bin/Sys
-rm -rf ./AppDir/usr/share/dolphin-emu
-sed -i 's/env QT_QPA_PLATFORM=xcb dolphin-emu/dolphin-emu/g' ./AppDir/usr/share/applications/dolphin-emu.desktop
+rm ./AppDir/usr/bin/dolphin-mpn-nogui
+rm ./AppDir/usr/bin/dolphin-tool
+mv ./AppDir/usr/share/dolphin-mpn/sys ./AppDir/usr/bin/Sys
+rm -rf ./AppDir/usr/share/dolphin-mpn
+sed -i 's/env QT_QPA_PLATFORM=xcb dolphin-mpn/dolphin-mpn/g' ./AppDir/usr/share/applications/dolphin-mpn.desktop
 
 # Prepare Tools for building the AppImage
-wget -N https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
-wget -N https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
-wget -N https://github.com/linuxdeploy/linuxdeploy-plugin-checkrt/releases/download/continuous/linuxdeploy-plugin-checkrt-x86_64.sh
-wget -N https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
+wget -N https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-${ARCH}.AppImage
+wget -N https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-${ARCH}.AppImage
+wget -N https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-${ARCH}.AppImage
 
 
-chmod a+x linuxdeploy-x86_64.AppImage
-chmod a+x linuxdeploy-plugin-checkrt-x86_64.sh
-chmod a+x appimagetool-x86_64.AppImage
+chmod a+x linuxdeploy-${ARCH}.AppImage
+chmod a+x linuxdeploy-plugin-qt-${ARCH}.AppImage
+chmod a+x appimagetool-${ARCH}.AppImage
 
 # Build the AppImage
-./linuxdeploy-x86_64.AppImage \
+./linuxdeploy-${ARCH}.AppImage \
   --appdir AppDir \
-  --plugin checkrt
+  --plugin qt
 
-./appimagetool-x86_64.AppImage -n ./AppDir root/
+echo 'env QT_QPA_PLATFORM=xcb' >> ./AppDir/apprun-hooks/linuxdeploy-plugin-qt-hook.sh
+
+./appimagetool-${ARCH}.AppImage ./AppDir
